2c7bad8c78769c55ffae7da9cad1bd6a62e1aed4
kajonsak <johnykdbibi@gmail.com>
2020-11-23 15:01:04 +0700

    LAB 23/11/2020

---
+++ lab11.java
@@ -0,0 +1,22 @@
+package com.john.HelloWorld;
+
+public class lab11 {
+public int sum(int a,int b) {
+		
+		return a+b;
+	}
+	
+	public void printName() {
+		System.out.println("KAJONSAK");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		lab11 a = new lab11();
+		
+		System.out.println(a.sum(5, 7));
+		a.printName();
+
+	}
+
+}
